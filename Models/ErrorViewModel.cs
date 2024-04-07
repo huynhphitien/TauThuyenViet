da7ce3d2b5aed9b7ed6317c4149600e030b6ed49
{Tien} <{huynhphitienhpt@gmail.com}>
2024-04-07 17:39:26 +0700

    upload base

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TauThuyenMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
